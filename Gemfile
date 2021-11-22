635e239abba876a06bfa43ab8f5eaf8cbd8eb672
t-hirata74 <t.hirata.0463@gmail.com>
2021-11-22 16:02:28 +0900

    setup test

--- Gemfile
+++ Gemfile
@@ -45,5 +45,10 @@ group :development do
   gem 'spring-watcher-listen', '~> 2.0.0'
 end
 
+group :test do
+  # テスト結果を色付けする
+  gem 'minitest-reporters', '~> 1.4', '>= 1.4.3'
+end
+
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,8 +2,21 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
+# gem minitest-teporters setup
+require "minitest/reporters"
+Minitest::Reporters.use!
+
 class ActiveSupport::TestCase
+  # 並列テスト ... 複数のプロセスを分岐させテスト時間の短縮を行う機能
+  # 
+  # プロセスが分岐した直後に呼び出し
+  parallelize_setup do |worker|
+    # seedデータの読み込み
+    load "#{Rails.root}/db/seeds.rb"
+  end
   # Run tests in parallel with specified workers
+  # 並列テストの有効化・無効化
+  # workers: プロセス数を渡す(2以上 => 有効、2未満 => 無効)
   parallelize(workers: :number_of_processors)
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
