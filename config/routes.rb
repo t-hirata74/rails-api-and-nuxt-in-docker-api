36929051e33a9dcc28068d50d869909aefb39df2
t-hirata74 <t.hirata.0463@gmail.com>
2021-11-22 19:02:57 +0900

    fix DockerFile & code

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      # api test action
-      resources :hello, only:[:index]
+      # user_controller
+      resources :users, only:[:index]
     end
   end
 end
